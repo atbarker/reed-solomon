2a3f887e75d70b23de6a6094a73a693e4bd4c1f5
austen barker <atbarker@ucsc.edu>
2019-02-28 19:19:42 -0800

    added lookup table for log and exp

--- src/rs.h
+++ src/rs.h
@@ -3,8 +3,17 @@
 
 #include <stdint.h>
 
-uint8_t init_tables(uint16_t prim_poly);
+//tables for storing Galois field operation results, 2D arrays if requiring two operands, so 64KB each
+uint8_t gf_mul_table[256][256];
+uint8_t gf_inv_table[256];
+uint8_t gf_pow_table;
+uint8_t gf_div_table;
 
 uint8_t gf_add(uint8_t x, uint8_t y);
+uint8_t gf_mult_table(uint8_t x, uint8_t y);
+uint8_t gf_div(uint8_t x, uint8_t y);
+uint8_t gf_pow(uint8_t x, uint8_t pow);
+uint8_t gf_inv(uint8_t x);
+
 
 #endif
