eadafb5f18c453b1e1096d36f812e8e6626d6930
austen barker <atbarker@ucsc.edu>
2019-02-28 17:32:56 -0800

    woop

--- src/polynomial.h
+++ src/polynomial.h
@@ -2,10 +2,38 @@
 #define POLY_H
 
 typedef struct{
-    int size;
-    int offset;
-    int length;
-    uint8_t* byte_array;
-} polynomial;
+    int size; //size of the polynomial
+    int offset; //offset
+    int length; //length of the byte array
+    uint8_t* byte_array; //actual storage of the byte array for the polynomial
+} Polynomial;
+
+
+//assumes an already malloc'd byte array to the desired length (max should be 256 so 8 bits is enough)
+uint32_t init(uint8_t size, uint8_t offset, uint8_t length, uint8_t* byte_array);
+
+//append to an existing polynomial
+uint32_t append(Polynomial* p, uint8_t x);
+
+//reset values to zero, really just zero the size, don't have to zero the memory
+uint32_t reset(Polynomial* p);
+
+//set a polynomial to existing values
+uint32_t set(Polynomial* p, uint8_t byte_seq, uint8_t len, uint8_t offset);
+
+//make a copy of a polynomial
+uint32_t copy(Polynomial* src, Polynomial* dest);
+
+//return the length of the length of the byte array
+uint8_t length(Polynomial* p);
+
+//return the size of polynomial
+uint8_t size(Polynomial* p);
+
+//coefficient at position i
+uint8_t value_at(Polynomial* p, uint32_t i);
+
+//return pointer to byte array
+uint8_t mem(Polynomial* p);
 
 #endif
