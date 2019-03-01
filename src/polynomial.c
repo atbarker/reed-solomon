c9afa6e5f36927b74c6f14ae11b2b7df5df30154
austen barker <atbarker@ucsc.edu>
2019-02-28 17:14:03 -0800

    added poly.h

--- src/polynomial.c
+++ src/polynomial.c
@@ -1,7 +1,2 @@
 #include <stdint.h>
-
-typedef struct {
-    int size; //size in memory
-    int offset; //internal offset
-    int length; //polynomial length
-} polynomial;
+#include "polynomial.h"
---
+++ src/polynomial.h
@@ -0,0 +1,11 @@
+#ifndef POLY_H
+#define POLY_H
+
+typedef struct{
+    int size;
+    int offset;
+    int length;
+    uint8_t* byte_array;
+} polynomial;
+
+#endif
