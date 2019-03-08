a658461ba58c6119a56608ba30e161c4fa9a7f3d
austen barker <atbarker@ucsc.edu>
2019-03-07 18:51:50 -0800

    kernel test ready

--- kernel/main.c
+++ kernel/main.c
@@ -12,7 +12,32 @@ MODULE_LICENSE("MIT");
 MODULE_AUTHOR("AUSTEN BARKER");
 
 static int __init km_template_init(void){
+    uint8_t parity[32];
+    uint8_t data[223];
+    uint8_t corrupted_data[223];
+    uint8_t output[255];
+    uint8_t errors[1] = {0};
+    int i;
+
     printk(KERN_INFO "Inserting kernel module\n");
+
+    get_random_bytes(data, 223);
+
+    encode(data, 223, parity, 32);
+
+    memcpy(corrupted_data, data, 223);
+    corrupted_data[0] = 0;
+
+    decode(corrupted_data, parity, 223, 32, output, errors, 1);
+
+    for(i = 0; i < 223; i++){
+        if(output[i] != data[i]){
+            printk(KERN_INFO "Decode failed at character %d\n", i);
+	    goto exit;
+	}
+    }
+    printk(KERN_INFO "Decode completed successfully \n");
+exit:
     return 0;
 }
 
